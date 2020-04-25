135a02b1175401fa34db6901e9ef5578bf49d971
Rohil Tuli <rohil.tuli@gmail.com>
2020-04-25 12:18:43 -0400

    Worked on popover

--- src/FrontPage/index.tsx
+++ src/FrontPage/index.tsx
@@ -1,9 +1,29 @@
 import React, { Component } from 'react';
+import { Popover, Row, Col, Avatar } from 'antd';
+import { ShoppingCartOutlined, HomeOutlined } from '@ant-design/icons';
 import './frontpage.css';
-function FrontPage() {
+
+
+const FrontPage = () => {
     return (
         <div className="background">
-            Hello
+            <Row>
+                <Col span={6}/>
+                <Col span={6}>
+                    <Popover content={<div>Click here to see who needs help near you</div>} title="Deliver groceries">
+                        <Avatar size={128} icon={<ShoppingCartOutlined />} /> 
+                    </Popover>
+                </Col>
+                <Col span={6}>
+                <Popover content={<div>Click here to see who needs help near you</div>} title="Deliver groceries">
+
+                    <Avatar size={128} icon={<HomeOutlined />} />
+                    </Popover>
+                    
+                </Col>
+                <Col span={6}/>
+            </Row>
+            Hello there
         </div>
     )
 }
