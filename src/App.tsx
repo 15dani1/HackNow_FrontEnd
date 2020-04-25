755c0e7d6c44eecdc0c70af24ca96c9f81bbaf2a
Vishvak Seenichamy <vishvak365@gmail.com>
2020-04-25 00:30:00 -0400

    Convert to functions

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Button from 'antd/es/button';
 import './App.css';
+import FrontPage from './FrontPage'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Button type="primary">Button</Button>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <FrontPage />
+  );
 }
 
 export default App;
---
+++ src/FrontPage/index.tsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './frontpage.css';
+function FrontPage() {
+    return (
+        <div className="background">
+            Hello
+        </div>
+    )
+}
+export default FrontPage;
