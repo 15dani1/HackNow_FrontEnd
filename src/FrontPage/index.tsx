e4f2e380fbb7e21c97d7dfecdfe4f845172abcb9
Vishvak Seenichamy <vishvak365@gmail.com>
2020-04-25 01:37:39 -0400

    Basic Visual Implementation Front page

--- src/FrontPage/index.tsx
+++ src/FrontPage/index.tsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import oldMan from "../images/oldMan.jpg"
+import youngDude from "../images/youngdude.jpg"
 import './frontpage.css';
 import { Card } from 'antd';
 function FrontPage() {
@@ -6,10 +8,12 @@ function FrontPage() {
         <div>
             <div className="card-div">
                 <Card className="card-css" hoverable={true}>
-                    <p>Card content</p>
+                    <img className="image-Card-CSS" src={youngDude} />
+                    <h2 className="path">Be The Driver!</h2>
                 </Card>
                 <Card className="card-css" hoverable={true}>
-                    <p>Card content</p>
+                    <img className="image-Card-CSS" src={oldMan} />
+                    <h2 className="path">I Need Assistance!</h2>
                 </Card>
             </div>
         </div >
