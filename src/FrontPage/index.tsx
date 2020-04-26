ab319206dab926c01bc61c843dc3c9beb7a66d2a
Rohil Tuli <rohil.tuli@gmail.com>
2020-04-26 10:58:04 -0400

    Finished user login I think

--- src/FrontPage/index.tsx
+++ src/FrontPage/index.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import oldMan from "../images/oldMan.jpg"
 import youngDude from "../images/youngdude.jpg"
 import './frontpage.css';
-import { Card, Input, Button } from 'antd';
+import { Card, Input, Button, Modal, message } from 'antd';
 import MapPage from '../mapPage';
 import axios from 'axios';
 import RequestPage from '../requestPage';
@@ -15,7 +15,10 @@ const FrontPage = () => {
     const [userType, setUserType] = useState(false);
     const [phoneNumber, setPhoneNumber] = useState("");
     const [name, setName] = useState("");
-    const [address, setAddress] = useState("")
+    const [address, setAddress] = useState("");
+    const [inputName, setInputName] = useState("");
+    const [inputAddress, setInputAddress] = useState("");
+    const [showCreateModal, setShowCreateModal] = useState(false);
     const nextPage = () => {
         if (page == 0) {
             setPage(1)
@@ -36,6 +39,31 @@ const FrontPage = () => {
         setUserType(true);
         setPage(2);
     }
+    const handleCreate = () => {
+        axios.post("http://localhost:5000/account", {
+            query: `mutation($phoneNumber: String!, $name: String!, $address: String!) {
+                createAccount(Name: $name, Address: $address, PhoneNumber: $phoneNumber){
+                    name
+                    address
+                    phoneNumber
+                }
+            }`,
+            variables: {
+                phoneNumber: phoneNumber,
+                address: inputAddress,
+                name: inputName
+            }
+        }, {
+            headers: {
+              'Content-Type': 'application/json'
+        }}).then(res => {
+            console.log(res);
+            setAddress(res.data.data.createAccount.address)
+            setName(res.data.data.createAccount.name)
+            setShowCreateModal(false);
+            message.success("Logged in!")
+        });
+    }
     const handleLogin = () => {
         axios.post("http://localhost:5000/account", {
             query: `query($phoneNumber: String!) {
@@ -55,11 +83,31 @@ const FrontPage = () => {
             console.log(res.data)
             setName(res.data.data.account.name);
             setAddress(res.data.data.account.address);
+            res.data.data.account.address ? message.success("Logged in!") : setShowCreateModal(true);
         });
     }
     return (
         page === 0 ?
             <div>
+                <Modal
+                title="Create account"
+                visible={showCreateModal}
+                onOk={() => {}}
+                onCancel={() => {}}
+                footer={[
+                    <Button key="back" onClick={() => setShowCreateModal(false)}>
+                    Return
+                    </Button>,
+                    <Button key="submit" type="primary" onClick={handleCreate}>
+                    Create Account and Login
+                    </Button>,
+                ]}
+                width={"75%"}>
+                    <Input placeholder="Name"
+                        value={inputName} onChange={e => setInputName(e.target.value)}/>
+                    <Input placeholder="Address"
+                        value={inputAddress} onChange={e => setInputAddress(e.target.value)}/>
+                </Modal>
                 <Search placeholder="Phone number" enterButton="Submit"
                     value={phoneNumber}
                     onChange={e => setPhoneNumber(e.target.value)}
